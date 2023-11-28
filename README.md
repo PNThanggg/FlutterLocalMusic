939a8c027b841843b97e714f8a49b9d819a7883d
PNT <92286791+PNThanggg@users.noreply.github.com>
2023-11-29 00:42:00 +0700

    :tada: project flutter 3.16.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# local_music
+
+Local Music App
+
+## Getting Started
+
+Get local music
+
+Support only Android
---
+++ android/app/src/main/kotlin/com/pnt/local_music/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pnt.local_music
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
